68e3335b7205c0fc1fa427c8c4f80c6c847f7e7c
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-05-27 19:32:44 -0700

    fix: changes

--- {{cookiecutter.project_name}}/scripts/deploy.py
+++ {{cookiecutter.project_name}}/scripts/deploy.py
@@ -5,3 +5,8 @@ from ape.cli import get_user_selected_account
 def main():
     account = get_user_selected_account()
     account.deploy(project.Token)
+
+# check it out on rebki
+# opensea on testnet
+# show the collection registered
+# 
