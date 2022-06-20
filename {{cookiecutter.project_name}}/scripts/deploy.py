ace77fda2c20d205a32022caac3f1bcff765c19f
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-06-21 00:17:00 +0200

    chore: update project name (#11)

--- {{cookiecutter.project_name}}/scripts/deploy.py
+++ {{cookiecutter.project_name}}/scripts/deploy.py
@@ -4,9 +4,4 @@ from ape.cli import get_user_selected_account
 
 def main():
     account = get_user_selected_account()
-    account.deploy(project.Token)
-
-# check it out on rebki
-# opensea on testnet
-# show the collection registered
-# 
+    account.deploy(project.NFT)
