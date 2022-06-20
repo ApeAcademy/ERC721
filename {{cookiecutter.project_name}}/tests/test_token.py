811bbfc2a57d951cbf83473029f0d15946de6b8c
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2022-06-20 17:12:50 -0400

    refactor: fix default test case and add erc165 one

--- {{cookiecutter.project_name}}/tests/test_token.py
+++ {{cookiecutter.project_name}}/tests/test_token.py
@@ -1,12 +1,28 @@
 import ape
-from vyper.interfaces import ERC721
-
-implements: ERC721
 
 
+def test_erc165(nft):
+    # ERC165 interface ID of ERC165
+    assert nft.supportsInterface("0x01ffc9a7")
 
-def test_no_portfolio(owner, accounts):
-    a = accounts[0]
-    assert owner.balanceOf(a) == 0
+    # ERC165 specifies that this is never supported
+    assert not nft.supportsInterface("0xffffffff")
+
+    # ERC165 interface ID of ERC721
+    assert nft.supportsInterface("0x80ac58cd")
+{%- if cookiecutter.metadata == 'y' %}
+
+    # ERC165 interface ID of ERC721 Metadata Extension
+    assert nft.supportsInterface("0x5b5e139f")
+{%- endif %}
+{%- if cookiecutter.permitable == 'y' %}
+
+    # ERC165 interface ID of ERC4494
+    assert nft.supportsInterface("0x5604e225")
+{%- endif %}
+
+
+def test_init(nft, owner):
+    assert nft.balanceOf(owner) == 0
     with ape.reverts():
-        owner.estimatedValue(0)
+        assert nft.ownerOf(0)
