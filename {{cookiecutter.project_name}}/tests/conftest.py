146b00582cb342cad4c70aad3e8bb4b93924fa9d
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2022-06-20 17:12:38 -0400

    fix: update conftest for nft

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -5,31 +5,12 @@ import pytest
 def owner(accounts):
     return accounts[0]
 
+
 @pytest.fixture(scope="session")
 def receiver(accounts):
     return accounts[1]
 
+
 @pytest.fixture(scope="session")
-def token(owner, project):
-    return owner.deploy(project.Token)
-
-
-@pytest.fixture
-def sudo(accounts):
-    return accounts[-1]
-
-
-@pytest.fixture
-def token(project, sudo):
-    return sudo.deploy(project.TestToken)
-
-
-
-@pytest.fixture
-def mint_tokens(token, sudo):
-    def mint_tokens(account):
-        token.mint(account, "100 ether", sender=sudo)
-
-    return mint_tokens
-
-
+def nft(owner, project):
+    return owner.deploy(project.NFT)
